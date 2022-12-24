df92b1db9b907fcc47eed76ef69ee8687c5f286d
Helge Stegemoen <helge@stegemoen.com>
2022-12-24 17:01:14 +0100

    SQL reference scripts

---
+++ scripts/create_employee_table.sql
@@ -0,0 +1,9 @@
+ USE sb_timetable;
+ create table Employee
+    (
+  employee_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name varchar(50),
+last_name varchar(50),
+Email varchar(255),
+Password varchar(50)
+);
---
+++ scripts/create_project_with_foreignKeys.sql
@@ -0,0 +1,10 @@
+ USE sb_timetable;
+CREATE TABLE Project (
+	project_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    project_name varchar(255),
+    active boolean,
+    employee_id int,
+    company_id int,
+    FOREIGN KEY (employee_id) REFERENCES Employees(employee_id),
+    FOREIGN KEY (company_id) REFERENCES Companies(company_id)
+);
